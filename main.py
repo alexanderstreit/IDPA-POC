44f015e790c6b77d1f609379b8c246ef03b9db89
alex <alexander.streit@wksbildung.ch>
2019-02-23 13:12:11 +0100

    Finalisierungen

--- main.py
+++ main.py
@@ -12,20 +12,21 @@ counter = 0
 
 with open('data.csv', mode='w') as file:
     writer = csv.writer(file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(['Zeit','Temperatur', 'Druck', 'Luftfeuchtigkeit', 'Orientierung (Degrees)', 'Kompass (Norden)', 'Beschl. X', 'Beschl. Y', 'Beschl. Z', 'Temperatur (Feuchtigkeit)', 'Druck 2', 'Luftfeuchtigkeit 2', 'Temparatur (Druck)'])
+    writer.writerow(['Zeit','Temperatur1', 'Temperatur2', 'Temperatur3', 'Luftdruck', 'Luftfeuchtigkeit', 'Yaw', 'Pitch', 'Roll', 'Compass X', 'Compass Y', 'Compass Z', 'Gyro X', 'Gyro Y', 'Gyro Z'])
 
 with open('acc.csv', mode='w') as file:
     writer = csv.writer(file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(['Zeit','X','Y','Z'])
+    writer.writerow(['Zeit','Acc_X','Acc_Y','Acc_Z'])
 
 # Farben definieren
 red = (255, 0, 0)
 green = (0, 255, 0)
+black = (0,0,0)
 
-def writeToCsv(temp, press, hum, pos_deg, pos_comp, temp2, press2, humm2, temp3):
+def writeDataToCsv(temperature, temperature2, temperature3, pressure, humidty, yaw, pitch, roll, mag_x, mag_y, mag_z, gyro_x, gyro_y, gyro_z):
     with open('data.csv', mode='a') as file:
         writer = csv.writer(file, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow([datetime.datetime.now(),temp, press, hum, pos_deg, pos_comp, temp2, press2, humm2, temp3])
+        writer.writerow([datetime.datetime.now(),temperature, temperature2, temperature3, pressure, humidty, yaw, pitch, roll, mag_x, mag_y, mag_z, gyro_x, gyro_y, gyro_z])
     print('data geschrieben')
     
 def writeAccelerationToCsv(x,y,z):
@@ -36,10 +37,8 @@ def writeAccelerationToCsv(x,y,z):
     
 def main():
     try:
-        while True:
-            print('t')
+        while False:
             #Region Acceleration
-            tempp = 2/0
             acceleration = sense.get_accelerometer_raw()
             acc_x = acceleration['x']
             acc_y = acceleration['y']
@@ -49,17 +48,31 @@ def main():
             counter+=1
             
             #Region Data
-            if(counter == 4):
-                temp = sense.get_temperature()
-                press = sense.get_pressure()
-                hum = sense.get_humidity()   
-                pos_deg = sense.get_orientation_degrees()
-                temp2 = sense.get_temperature_from_humidity()
-                temp3 = sense.get_temperature_from_pressure()
-                press2 = 0
-                humm2 = 0        
-                comp = sense.compass
-                writeToCsv(temp, press, hum, pos_deg, comp, temp2, press2, humm2, temp3)
+            if(counter == 4):                
+                temperature = sense.get_temperature()
+                temperature2 = sense.get_temperature_from_humidity()
+                temperature3 = sense.get_temperature_from_pressure()
+                
+                pressure = sense.get_pressure()
+                humidty = sense.get_humidity()
+                
+                orientation = sense.get_orientation()
+                yaw = orientation["yaw"]
+                pitch = orientation["pitch"]
+                roll = orientation["roll"]
+                
+                mag = sense.get_compass_raw()
+                mag_x = mag["x"]
+                mag_y = mag["y"]
+                mag_z = mag["z"]
+                
+                gyro = sense.get_gyroscope_raw()
+                gyro_x = gyro["x"]
+                gyro_y = gyro["y"]
+                gyro_z = gyro["z"]
+                
+                writeDataToCsv(temperature, temperature2, temperature3, pressure, humidty, yaw, pitch, roll, mag_x, mag_y, mag_z, gyro_x, gyro_y, gyro_z)
+                
                 counter = 0;
     except Exception as e:
         with open('log.csv', mode='a') as file:
