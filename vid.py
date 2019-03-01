b759a0d1d9a90b326dfd54b801ed37d20c6d5b26
alex <alexander.streit@wksbildung.ch>
2019-03-01 23:08:31 +0100

    videos

---
+++ vid.py
@@ -0,0 +1,60 @@
+from picamera import PiCamera
+from sense_hat import SenseHat
+from time import sleep
+import time
+import csv
+
+BLUE = (0, 0, 255)
+GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+
+
+class VideoLogger(object):
+    
+    def __init__(self):
+        delimiter=';'
+        quotechar='"'
+        quoting=csv.QUOTE_MINIMAL
+        self.camera = PiCamera()
+        self.camera.resolution = (1280, 720)
+        self.camera.framerate = 30        
+        self.sense = SenseHat()
+        log_headings = ['Zeit','Fehler']
+        with open('video_log.csv', 'w') as file:
+            writer = csv.writer(file, delimiter=delimiter, quotechar=quotechar, quoting=quoting)
+            writer.writerow(log_headings)
+        self.sense.set_pixel(6, 7, BLACK)
+        self.sense.set_pixel(7, 7, BLACK)
+        
+    def write_data_to_file(self, data, file_name, mode='a', delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL):
+        with open(file_name, mode=mode) as file:
+            writer = csv.writer(file, delimiter=delimiter, quotechar=quotechar, quoting=quoting)
+            for row in data:
+                writer.writerow(row)
+        
+    def start_filming(self):
+        counter = 1
+        while True:
+            self.camera.start_recording('/home/pi/pi-strato-flight/videos/video' + str(counter) + '.h264')
+            self.sense.set_pixel(7, 7, GREEN)
+            print('starting')
+            self.camera.wait_recording(60)
+            self.camera.stop_recording()
+            self.sense.set_pixel(7, 7, BLACK)
+            counter += 1
+            
+    def log_exception(self, exception):
+        print(str(exception))
+        self.sense.set_pixel(6, 7, RED)
+        row = []
+        row.append([str(datetime.datetime.now()), str(exception)])
+        self.write_data_to_file(row, 'video_log.csv')
+            
+        
+if __name__ == '__main__':
+    logger = VideoLogger()
+    try:
+        logger.start_filming()
+    except Exception as e:
+        logger.log_exception(e)
